b12c48ef0757eef7aa5fb4efd1c3f4992eb5518c
Muzalev Dmitri <dielf2014@yandex.ru>
2023-11-23 17:25:14 +0300

    start api

---
+++ app.py
@@ -0,0 +1,33 @@
+import requests
+
+API_URL = 'https://api-metrika.yandex.ru/stat/v1/data'
+API_token = 'y0_AgAAAAAQzG58AAreeAAAAADyv60h_zECZQYJQMO_7FDfl9nxLRPLOso'
+params = {
+    'date1': '10daysAgo',
+    'date2': 'today',
+    'id': 95281988,
+    'metrics': 'ym:s:visits,ym:s:users',
+    'dimensions': 'ym:s:TrafficSource',
+    'filters': "ym:s:isRobot=='No'",
+    'limit': 1000
+}
+
+# Выполняем запрос к API
+response = requests.get(API_URL, params=params, headers={'Authorization': API_token})
+
+# Проверяем успешность запроса
+if response.status_code == 200:
+    # Извлекаем данные из ответа
+    data = response.json().get('data', [])
+
+    # Проверяем, есть ли данные в ответе
+    if data:
+        for item in data:
+            print(f"Источник трафика: {item['dimensions'][0]['name']}")
+            print(f"Количество посещений: {item['metrics'][0]}")
+            print(f"Количество пользователей: {item['metrics'][1]}")
+            print("-" * 30)
+    else:
+        print('Данные не найдены в ответе API.')
+else:
+    print(f'Ошибка при запросе к API. Код ответа: {response.status_code}')
